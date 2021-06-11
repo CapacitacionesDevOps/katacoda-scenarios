b60584695ed15dc1a8665c92065cbfd9b65d1808
Edison Danilo Chica Botero <echicabo@contratista.epm.co>
2021-06-11 09:51:47 -0500

    Se agrega configuración inicial del foreground al step que carga la sesión 1

--- git/fixing-merge-conflicts/step1-foreground.sh
+++ git/fixing-merge-conflicts/step1-foreground.sh
@@ -1,15 +1,19 @@
 number_random=$RANDOM
 file="merge_$number_random.txt"
-git clone https://capacitaciones-azure-devops@dev.azure.com/capacitaciones-azure-devops/Aprendiendo_Git/_git/Aprendiendo_Git
-sleep 2
+mkdir Aprendiendo_Git
 cd Aprendiendo_Git
-git branch "features/merge_$number_random"
-echo 'console.log("Modificado por otro desarrollador");' > $file
-git add $file
-git commit -m "Commit Inicial"
-git push https://dev.azure.com/capacitaciones-azure-devops/Aprendiendo_Git/_git/Aprendiendo_Git master
-sleep 2
-git checkout "features/merge_$number_random"
+git init
 echo 'console.log("Modificado por mi");' > $file
 git add $file
 git commit -m "Commit Inicial"
+sleep 3
+git checkout -b "feature/cambio_archivo"
+sleep 3
+echo 'console.log("Modificado por otro desarrollador");' > $file
+git add $file
+git commit -m "Commit Inicial de la rama"
+sleep 3
+git checkout "master"
+echo 'console.log("Modificado por mi nuevamente");' > $file
+git add $file
+git commit -m "Se modifica el archivo por mi, nuevamente"
